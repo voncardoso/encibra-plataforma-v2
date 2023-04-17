d7a67e64ebc63570ce6892034d1ecfa701a37c63
voncardoso <vonharrison26@gmail.com>
2023-04-17 16:25:41 -0300

    route criate

--- src/Pages/Rodovia/RoadCore/index.jsx
+++ src/Pages/Rodovia/RoadCore/index.jsx
@@ -1,3 +1,21 @@
+import { NavLink, useParams } from "react-router-dom";
+
 export function RoadCore() {
-  return <h1>Teste</h1>;
+  const params = useParams();
+  console.log(params);
+  return (
+    <section className="w-full overflow-y-scroll ">
+      <header className="flex justify-between pr-5 pt-8 pb-8 items-center">
+        <nav>
+          <NavLink to={"/rodovias"}>
+            <strong className="text-2xl">Rodovias </strong>
+          </NavLink>
+          <NavLink to={"/rodovias"}>
+            <strong className="text-2xl"> NR-{params.id}</strong>
+          </NavLink>
+        </nav>
+        <input type="text" />
+      </header>
+    </section>
+  );
 }
