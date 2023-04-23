d53c3edd1b671eb53779133625a7ffb1a93c8833
voncardoso <vonharrison26@gmail.com>
2023-04-23 20:25:53 -0300

    feat-ajuste-nav-rodovia

--- src/components/DefaultLayoutRoad/index.jsx
+++ src/components/DefaultLayoutRoad/index.jsx
@@ -1,39 +1,50 @@
 import { BrowserRouter, NavLink, Outlet, useParams } from "react-router-dom";
 import { Nav } from "./style";
 
-export function DefaultLayoutRoad(){
-    const params = useParams();
-    return(
-        <section className="gap-5 bg-background h-screen overflow-y-scroll w-full pr-5 ">
-            <div>
-                <header className="flex justify-between pr-5 pt-5 pb-5 items-center text-text-100">
-                    <nav>
-                    <NavLink to={"/rodovias"}>
-                        <strong className="text-2xl hover:underline">Rodovias </strong>
-                    </NavLink>
-                    <NavLink to={`/rodovias/nucleo/${1}`}>
-                        <strong className="text-2xl hover:underline"> | NR-01</strong>
-                    </NavLink>
-                    <strong className="text-2xl">|</strong>
-                    <NavLink to={`#`}>
-                        <strong className="text-2xl hover:underline"> PA-999</strong>
-                    </NavLink>
-                    </nav>
-                </header>
-            </div>
+export function DefaultLayoutRoad() {
+  const params = useParams();
+  console.log(params);
+  return (
+    <section className="gap-5 bg-background h-screen overflow-y-scroll w-full pr-5 ">
+      <div>
+        <header className="flex justify-between pr-5 pt-5 pb-5 items-center text-text-100">
+          <nav>
+            <NavLink to={"/rodovias"}>
+              <strong className="text-2xl hover:underline">Rodovias </strong>
+            </NavLink>
+            <NavLink to={`/rodovias/nucleo/${1}`}>
+              <strong className="text-2xl hover:underline"> | NR-01</strong>
+            </NavLink>
+            <strong className="text-2xl">|</strong>
+            <NavLink to={`#`}>
+              <strong className="text-2xl hover:underline">{params.id}</strong>
+            </NavLink>
+          </nav>
+        </header>
+      </div>
 
-            
-            <Nav className="w-full flex gap-5 border-b border-gray-400 text-gray-400 py-1">
-                <NavLink exact className="" to={`/rodovias/information/${"PA-999"}`}>Informações</NavLink>
-                <NavLink className="" to={`/rodovias/municipios/${"PA-999"}`}>Municípios</NavLink>
-                <NavLink className="" to={`/rodovias/pontos/${"PA-999"}`}>Pontos</NavLink>
-                <NavLink className="" to={`/rodovias/resvestimento/${"PA-999"}`}>Revestimentos</NavLink>
-                <NavLink className="" to={`/rodovias/videos/${"PA-999"}`}>Videos</NavLink>
-                <NavLink className="" to={"/teste"}>Igg</NavLink>
-            </Nav>
-           
-            <Outlet />
-            
-        </section>
-    )
+      <Nav className="w-full flex gap-5 border-b border-gray-400 text-gray-400 py-1">
+        <NavLink exact className="" to={`/rodovias/information/${params.id}`}>
+          Informações
+        </NavLink>
+        <NavLink className="" to={`/rodovias/municipios/${params.id}`}>
+          Municípios
+        </NavLink>
+        <NavLink className="" to={`/rodovias/pontos/${params.id}`}>
+          Pontos
+        </NavLink>
+        <NavLink className="" to={`/rodovias/resvestimento/${params.id}`}>
+          Revestimentos
+        </NavLink>
+        <NavLink className="" to={`/rodovias/videos/${params.id}`}>
+          Videos
+        </NavLink>
+        <NavLink className="" to={"/teste"}>
+          Igg
+        </NavLink>
+      </Nav>
+
+      <Outlet />
+    </section>
+  );
 }
