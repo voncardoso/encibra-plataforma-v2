939f22c8cf556ee2fdb8f583a08a0d692297848d
voncardoso <vonharrison26@gmail.com>
2023-04-26 17:25:32 -0300

    feat: fuction update roadInformation

---
+++ src/Pages/Rodovia/RoadInformation/ModalUpdateCoordinates/index.jsx
@@ -0,0 +1,125 @@
+import { useForm } from "react-hook-form";
+import { X } from "@phosphor-icons/react";
+import { api } from "../../../../lib/api";
+import {
+  DialogOverlay,
+  DialogContent,
+  DialogPortal,
+  DialogClose,
+} from "@radix-ui/react-dialog";
+import { useParams } from "react-router-dom";
+
+export function ModalUpdateCoordinates(data) {
+  const params = useParams();
+  let strech = null;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    defaultValues: {},
+  });
+
+  if (data.data.stretch) {
+    strech = JSON.parse(data.data.stretch);
+  }
+
+  async function handleUpdateInformationCoordinates(dataUpdate) {
+    const token = window.localStorage.getItem("encibraapptoken-v2");
+
+    // objeto para atualizar a descrption
+    const strechUpdate = {
+      description: strech.description,
+      endLatitude: dataUpdate.endLatitude,
+      endLongitude: dataUpdate.endLongitude,
+      initialLatitude: dataUpdate.initialLatitude,
+      initialLongitude: dataUpdate.initialLongitude,
+    };
+
+    const response = await api.put(
+      `/road/${params.id}`,
+      {
+        stretch: `${JSON.stringify(strechUpdate)}`,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    if (response.status === 200) {
+      window.alert("Informações atualizados com sucesso");
+      window.location.reload();
+    }
+  }
+
+  return (
+    <DialogPortal>
+      <DialogOverlay className=" fixed inset-0 bg-black bg-opacity-50" />
+      <DialogContent className="fixed w-96 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-md shadow-lg p-4">
+        <header className="flex mb-5 justify-between">
+          <h1 className="text-xl text font-bold">Atualizar Município</h1>
+          <DialogClose asChild>
+            <button className="relative bottom-2">
+              <X />
+            </button>
+          </DialogClose>
+        </header>
+        <form
+          onSubmit={handleSubmit(handleUpdateInformationCoordinates)}
+          action=""
+        >
+          <label className="" htmlFor="initialLatitude">
+            Latitude inicial
+            <input
+              id="initialLatitude"
+              type="text"
+              defaultValue={strech?.initialLatitude}
+              className="mb-4 bg-gray-input w-full rounded-md p-2"
+              {...register("initialLatitude", { required: true })}
+            />
+          </label>
+
+          <label htmlFor="initialLongitude">
+            Longitude inicial
+            <input
+              type="text"
+              id="initialLongitude"
+              defaultValue={strech?.initialLongitude}
+              className="mb-4 bg-gray-input w-full rounded-md p-2"
+              {...register("initialLongitude", { required: true })}
+            />
+          </label>
+
+          <label className="" htmlFor="endLatitude">
+            Latitude final
+            <input
+              id="endLatitude"
+              type="text"
+              defaultValue={strech?.endLatitude}
+              className="mb-4 bg-gray-input w-full rounded-md p-2"
+              {...register("endLatitude", { required: true })}
+            />
+          </label>
+
+          <label className="" htmlFor="endLongitude">
+            Longitude final
+            <input
+              id="endLongitude"
+              type="text"
+              defaultValue={strech?.endLongitude}
+              className="mb-4 bg-gray-input w-full rounded-md p-2"
+              {...register("endLongitude", { required: true })}
+            />
+          </label>
+
+          <button className="mt-6 bg-gold-400 w-full p-2 text-white rounded hover:bg-gold-300">
+            Atualizar
+          </button>
+        </form>
+      </DialogContent>
+    </DialogPortal>
+  );
+}
--- src/Pages/Rodovia/RoadInformation/index.jsx
+++ src/Pages/Rodovia/RoadInformation/index.jsx
@@ -1,6 +1,10 @@
-import { PencilLine } from "@phosphor-icons/react";
 import { useContext, useEffect, useState } from "react";
 import { UserContextRoad } from "../../../Context/useContextRoad";
+import { useParams } from "react-router-dom";
+import { Dialog, DialogTrigger } from "@radix-ui/react-dialog";
+import { PencilLine } from "@phosphor-icons/react";
+import { ModalUpdate } from "./ModalUpdate";
+import { ModalUpdateCoordinates } from "./ModalUpdateCoordinates";
 
 export function RoadInformation() {
   const { dataRoad } = useContext(UserContextRoad);
@@ -10,84 +14,92 @@ export function RoadInformation() {
     strech = JSON.parse(dataRoad?.stretch);
   }
 
-  return (
-    <section className="">
-      <div className="flex flex justify-between items-end p-2.5 mt-5 bg-white rounded-md mt-2.5 shadow-lg ">
-        <div className="flex flex-col">
-          <strong className="py-2 text-text-100">
-            <span className="text-gray-400 tracking-wider mr-2">Rodovia:</span>
-            {dataRoad.acronym}
-          </strong>
-          <strong className="py-2 text-text-100 ">
-            <span className="text-gray-400 tracking-wider mr-2 ">Malha:</span>
-            {dataRoad.mesh}
-          </strong>
-          <strong className="py-2 text-text-100 ">
-            <span className="text-gray-400 tracking-wider mr-2 ">
-              Extensão:
-            </span>
-            {dataRoad.extention} Km
-          </strong>
-          <strong className="py-2 text-text-100 ">
-            <span className="text-gray-400 tracking-wider mr-2 ">
-              Núcleo Regional:
-            </span>
-            {dataRoad.regional}
-          </strong>
-          <strong className="py-2 text-text-100 ">
-            <span className="text-gray-400 tracking-wider mr-2 ">UF:</span>
-            {dataRoad.uf}
-          </strong>
-          <strong className="py-2 text-text-100 ">
-            <span className="text-gray-400 tracking-wider mr-2 ">
-              Descrição:
-            </span>
-            {strech?.description}
-          </strong>
+  if (dataRoad) {
+    return (
+      <section className="">
+        <div className="flex flex justify-between items-end p-2.5 mt-5 bg-white rounded-md mt-2.5 shadow-lg ">
+          <div className="flex flex-col">
+            <strong className="py-2 text-text-100">
+              <span className="text-gray-400 tracking-wider mr-2">
+                Rodovia:
+              </span>
+              {dataRoad.acronym}
+            </strong>
+            <strong className="py-2 text-text-100 ">
+              <span className="text-gray-400 tracking-wider mr-2 ">Malha:</span>
+              {dataRoad.mesh}
+            </strong>
+            <strong className="py-2 text-text-100 ">
+              <span className="text-gray-400 tracking-wider mr-2 ">
+                Extensão:
+              </span>
+              {dataRoad.extention} Km
+            </strong>
+            <strong className="py-2 text-text-100 ">
+              <span className="text-gray-400 tracking-wider mr-2 ">
+                Núcleo Regional:
+              </span>
+              {dataRoad.regional}
+            </strong>
+            <strong className="py-2 text-text-100 ">
+              <span className="text-gray-400 tracking-wider mr-2 ">UF:</span>
+              {dataRoad.uf}
+            </strong>
+            <strong className="py-2 text-text-100 ">
+              <span className="text-gray-400 tracking-wider mr-2 ">
+                Descrição:
+              </span>
+              {strech?.description}
+            </strong>
+          </div>
+          <div className="py-2 flex justify-end justify-self-end">
+            <Dialog>
+              <DialogTrigger className="flex text-sm justify-center items-center gap-1 p-1 text-sm text-sky-600 border rounded border-sky-600 hover:bg-sky-600 hover:text-white">
+                <PencilLine size={18} />
+              </DialogTrigger>
+              <ModalUpdate data={dataRoad} />
+            </Dialog>
+          </div>
         </div>
-        <div className="py-2 flex justify-end justify-self-end">
-          <button className="flex  gap-1 items-center  text-sky-600  rounded p-1 px-2 text hover:bg-sky-600 hover:text-white">
-            <PencilLine size={18} />
-            <p className="tracking-tight">Atualizar</p>
-          </button>
-        </div>
-      </div>
 
-      <h2 className="mt-5 mb-1 text-xl font-medium">Coordenadas</h2>
-      <div className="flex flex justify-between items-end p-2.5 bg-white rounded-md shadow-lg mb-5">
-        <div className="flex flex-col">
-          <strong className="py-2 text-text-100">
-            <span className="text-gray-400 tracking-wider mr-2">
-              Latitude inicial:
-            </span>
-            {strech?.initialLatitude}
-          </strong>
-          <strong className="py-2 text-text-100 ">
-            <span className="text-gray-400 tracking-wider mr-2 ">
-              Longitude inicial:
-            </span>
-            {strech?.initialLongitude}
-          </strong>
-          <strong className="py-2 text-text-100 ">
-            <span className="text-gray-400 tracking-wider mr-2 ">
-              Latitude final:
-            </span>
-            {strech?.endLatitude}
-          </strong>
-          <strong className="py-2 text-text-100 ">
-            <span className="text-gray-400 tracking-wider mr-2 ">
-              Latitude final:
-            </span>
-            {strech?.endLongitude}
-          </strong>
+        <h2 className="mt-5 mb-1 text-xl font-medium">Coordenadas</h2>
+        <div className="flex flex justify-between items-end p-2.5 bg-white rounded-md shadow-lg mb-5">
+          <div className="flex flex-col">
+            <strong className="py-2 text-text-100">
+              <span className="text-gray-400 tracking-wider mr-2">
+                Latitude inicial:
+              </span>
+              {strech?.initialLatitude}
+            </strong>
+            <strong className="py-2 text-text-100 ">
+              <span className="text-gray-400 tracking-wider mr-2 ">
+                Longitude inicial:
+              </span>
+              {strech?.initialLongitude}
+            </strong>
+            <strong className="py-2 text-text-100 ">
+              <span className="text-gray-400 tracking-wider mr-2 ">
+                Latitude final:
+              </span>
+              {strech?.endLatitude}
+            </strong>
+            <strong className="py-2 text-text-100 ">
+              <span className="text-gray-400 tracking-wider mr-2 ">
+                Latitude final:
+              </span>
+              {strech?.endLongitude}
+            </strong>
+          </div>
+          <div className="py-2 flex justify-end ">
+            <Dialog>
+              <DialogTrigger className="flex text-sm justify-center items-center gap-1 p-1 text-sm text-sky-600 border rounded border-sky-600 hover:bg-sky-600 hover:text-white">
+                <PencilLine size={18} />
+              </DialogTrigger>
+              <ModalUpdateCoordinates data={dataRoad} />
+            </Dialog>
+          </div>
         </div>
-        <div className="py-2 flex justify-end ">
-          <button className="flex  gap-1 items-center  text-sky-600  rounded p-1 px-2 text hover:bg-sky-600 hover:text-white">
-            <PencilLine size={18} />
-            <p className="tracking-tight">Atualizar</p>
-          </button>
-        </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 }
