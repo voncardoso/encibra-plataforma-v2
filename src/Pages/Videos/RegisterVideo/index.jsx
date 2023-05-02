a15d3560fd9b9f65be348aedaff42e3ffab53cba
Von Harrison <vonharrison26@gmail.com>
2023-05-02 15:24:46 -0300

    feat: register video finishd

--- src/Pages/Videos/RegisterVideo/index.jsx
+++ src/Pages/Videos/RegisterVideo/index.jsx
@@ -1,6 +1,14 @@
 import { useForm } from "react-hook-form";
+import { api } from "../../../lib/api";
+import { useContext } from "react";
+import { UserContextRoad } from "../../../Context/useContextRoad";
+import { useParams } from "react-router-dom";
+import { UserContextLogin } from "../../../Context/useContextLogin";
 
 export function RegisterVideo(){
+    const params = useParams()
+    const {dataRoad} = useContext(UserContextRoad)
+    const {} = useContext(UserContextLogin)
     const {
         register,
         handleSubmit,
@@ -10,9 +18,42 @@ export function RegisterVideo(){
       } = useForm({
         defaultValues: {},
       });
+
+      async function handleCreateVideo(data){
+        const token = window.localStorage.getItem("encibraapptoken-v2");
+        const userId = window.localStorage.getItem("encibraappId-v2");
+        const dataObjeto = new Date(data.date);
+        const video = {
+            url: "",
+            date: dataObjeto.toISOString(),
+            extension: data.extension,
+            kmInitial: data.kmInitial,
+            kmFinal: data.kmFinal,
+            core: dataRoad.regional,
+            processed: "PENDING",
+            stretch: data.stretch,
+            roadId: +params.id,
+            companyId: 1,
+            createdBy: +userId,
+            createdAt: new Date().toISOString(),
+            updatedAt: new Date().toISOString(),
+
+        }
+        console.log(video)
+        const response = await api.put(
+            `/road/${params.id}/videos/null/create`,
+            video,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+         
+      }
     return(
         <section className="  mt-5  flex overflow-auto w-full   border-b-8 border-background rounded-md">
-            <form className=" bg-white w-full h-auto p-5 grid grid-cols-3 gap-5" action="">
+            <form onSubmit={handleSubmit(handleCreateVideo)} className=" bg-white w-full h-auto p-5 grid grid-cols-3 gap-5" action="">
                 <h1 className="col-span-3 text-2xl font-bold text-text-100">
                     Cadastro de Rodovias
                 </h1>
@@ -22,40 +63,43 @@ export function RegisterVideo(){
                         id="stretch"
                         type="text"
                         className="bg-gray-input w-full rounded-md p-2"
-                        {...register("acronym", { required: true })}
+                        {...register("stretch", { required: true })}
                     />
                 </label>
-                <label htmlFor="extention">
+                <label htmlFor="extension">
                     Extensão
                     <input
                         type="text"
-                        id="extention"
+                        id="extension"
                         className="bg-gray-input w-full rounded-md p-2"
-                        {...register("extention", { required: true })}
+                        {...register("extension", { required: true })}
                     />
                 </label>
-                <label htmlFor="">
+                <label htmlFor="date">
                     Data
                     <input
                         type="date"
+                        id="date"
                         className="bg-gray-input w-full rounded-md p-2"
-                        {...register("acronym", { required: true })}
+                        {...register("date", { required: true })}
                     />
                 </label>
-                <label htmlFor="">
+                <label htmlFor="kmInitial">
                     Km ínicial
                     <input
                         type="text"
+                        id="kmInitial"
                         className="bg-gray-input w-full rounded-md p-2"
-                        {...register("acronym", { required: true })}
+                        {...register("kmInitial", { required: true })}
                     />
                 </label>
-                <label htmlFor="">
+                <label htmlFor="kmFinal">
                     Km final
                     <input
                         type="text"
+                        id="kmFinal"
                         className="bg-gray-input w-full rounded-md p-2"
-                        {...register("acronym", { required: true })}
+                        {...register("kmFinal", { required: true })}
                     />
                 </label>
                 <div className="col-end-2">
