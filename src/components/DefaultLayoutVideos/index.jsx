f9a8f017f95f1431fa030a9e9a5e42230eb4b2de
voncardoso <vonharrison26@gmail.com>
2023-05-03 11:10:24 -0300

    feat: ajuste default layout video

--- src/components/DefaultLayoutVideos/index.jsx
+++ src/components/DefaultLayoutVideos/index.jsx
@@ -1,15 +1,28 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useParams } from "react-router-dom";
 import { Nav } from "./style";
 
-export function DefaultLayoutVideos(){
-    return(
-        <section>
-            <Nav className="w-48 m-auto my-2.5 flex justify-center gap-10  py-1">
-                <NavLink exact className="  text-gray-400" to={`/rodovias/videos/${"PA-999"}/information/${2132}`}>Informações</NavLink>
-                <NavLink className=" text-gray-400" to={`/rodovias/videos/${"PA-999"}/patology/${2132}`}>Patologias</NavLink>
-            </Nav>
-           
-            <Outlet />
-        </section>
-    )
+export function DefaultLayoutVideos() {
+  const params = useParams();
+
+  return (
+    <section>
+      <Nav className="w-48 m-auto my-2.5 flex justify-center gap-10  py-1">
+        <NavLink
+          exact
+          className="text-gray-400"
+          to={`/rodovias/videos/${params.id}/information/${params.video}`}
+        >
+          Informações
+        </NavLink>
+        <NavLink
+          className="text-gray-400"
+          to={`/rodovias/videos/${params.id}/patology/${params.video}`}
+        >
+          Patologias
+        </NavLink>
+      </Nav>
+
+      <Outlet />
+    </section>
+  );
 }
