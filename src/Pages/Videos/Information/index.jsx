abbd8a1dc1e8f4701a15e1adc66e536fe7f8e1f7
voncardoso <vonharrison26@gmail.com>
2023-05-03 11:56:36 -0300

    feat: data video infomation add data

--- src/Pages/Videos/Information/index.jsx
+++ src/Pages/Videos/Information/index.jsx
@@ -1,44 +1,77 @@
-import { PencilLine } from "@phosphor-icons/react"
-import { useState } from "react"
-import { useParams } from "react-router-dom"
+import { PencilLine } from "@phosphor-icons/react";
+import { useContext, useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import { UserContextRoad } from "../../../Context/useContextRoad";
 
-export function Information(){
-    const {id, video} = useParams()
-    console.log(id, video)
-    return(
-        <div className="flex flex justify-between items-end p-2.5 mt-5 bg-white rounded-md mt-2.5 shadow-lg ">
-        <div className="flex flex-col">
-            <strong className="py-2 text-text-100">
-                <span className="text-gray-400 tracking-wider mr-2">Rodovia:</span>
-                Teste
-            </strong>
-            <strong className="py-2 text-text-100 ">
-                <span className="text-gray-400 tracking-wider mr-2 ">Malha:</span>
-                Teste
-            </strong>
-            <strong className="py-2 text-text-100 ">
-                <span className="text-gray-400 tracking-wider mr-2 ">Extensão:</span>
-                Teste    
-            </strong>
-            <strong className="py-2 text-text-100 ">
-                <span className="text-gray-400 tracking-wider mr-2 ">Núcleo Regional:</span>
-                Teste    
-            </strong>
-            <strong className="py-2 text-text-100 ">
-                <span className="text-gray-400 tracking-wider mr-2 ">UF:</span>
-                Teste
-            </strong>
-            <strong className="py-2 text-text-100 ">
-                <span className="text-gray-400 tracking-wider mr-2 ">Descrição:</span>
-                Teste
-            </strong>
-        </div>
-        <div className="py-2 flex justify-end justify-self-end">
-            <button className="flex  gap-1 items-center  text-sky-600  rounded p-1 px-2 text hover:bg-sky-600 hover:text-white">
-                <PencilLine size={18}/>
-                <p className="tracking-tight">Atualizar</p>
-            </button>
-        </div>
-    </div>
-    )
+export function Information() {
+  const { id, video } = useParams();
+
+  const { dataRoad } = useContext(UserContextRoad);
+
+  const dataVideos = dataRoad.videos?.filter(
+    (item) => item.id === Number(video)
+  );
+  console.log(dataVideos);
+  if (dataVideos) {
+    return (
+      <div className="flex flex justify-between items-end p-2.5 mt-5 bg-white rounded-md mt-2.5 shadow-lg ">
+        {dataVideos.map((video) => {
+          let dataString = video.date;
+          const data = new Date(dataString);
+          const dataUTC = data.toLocaleDateString("pt-BR", {
+            timeZone: "UTC",
+          });
+          return (
+            <>
+              <div className="flex flex-col">
+                <strong className="py-2 text-text-100">
+                  <span className="text-gray-400 tracking-wider mr-2">
+                    Trecho:
+                  </span>
+                  {video.stretch}
+                </strong>
+                <strong className="py-2 text-text-100 ">
+                  <span className="text-gray-400 tracking-wider mr-2 ">
+                    Data:
+                  </span>
+                  {dataUTC}
+                </strong>
+                <strong className="py-2 text-text-100 ">
+                  <span className="text-gray-400 tracking-wider mr-2 ">
+                    Extensão analisada:
+                  </span>
+                  {video.extension} Km
+                </strong>
+                <strong className="py-2 text-text-100 ">
+                  <span className="text-gray-400 tracking-wider mr-2 ">
+                    Km inicial:
+                  </span>
+                  {video.kmInitial}
+                </strong>
+                <strong className="py-2 text-text-100 ">
+                  <span className="text-gray-400 tracking-wider mr-2 ">
+                    Km final:
+                  </span>
+                  {video.kmFinal}
+                </strong>
+                <strong className="py-2 text-text-100 ">
+                  <span className="text-gray-400 tracking-wider mr-2 ">
+                    Status:
+                  </span>
+                  {(video.processed === "PENDING" && "Pendente") ||
+                    (video.processed === "PROCESSING" && "Em analise") ||
+                    (video.processed === "PROCESSED" && "Analisado")}
+                </strong>
+              </div>
+              <div className="py-2 flex justify-end justify-self-end">
+                <button className="flex text-sm justify-center items-center gap-1 p-1 text-sm text-sky-600 border rounded border-sky-600 hover:bg-sky-600 hover:text-white">
+                  <PencilLine size={18} />
+                </button>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    );
+  }
 }
